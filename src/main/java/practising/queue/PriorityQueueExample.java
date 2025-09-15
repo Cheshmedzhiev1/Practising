4abc1f53281741987d0823159bf1e6adbca76526
Cheshmedzhiev1 <martincheshmedzhiev@gmail.com>
2025-09-15 22:01:48 +0100

    Update PriorityQueueExample.java

--- src/main/java/practising/queue/PriorityQueueExample.java
+++ src/main/java/practising/queue/PriorityQueueExample.java
@@ -3,7 +3,7 @@ package practising.queue;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
-public class PriorityQueueExample  {
+public class PriorityQueueExample {
 
     public static void main(String[] args) {
 
@@ -13,10 +13,15 @@ public class PriorityQueueExample  {
         pq.add(20);
         pq.add(15);
 
+        System.out.println(pq);
         System.out.println(pq.peek());
 
-       System.out.println(pq.poll());
+        System.out.println(pq);
+        System.out.println(pq.poll());
 
-       System.out.println(pq.peek());
+        System.out.println(pq);
+        System.out.println(pq.peek());
+
+        System.out.println(pq);
     }
 }
